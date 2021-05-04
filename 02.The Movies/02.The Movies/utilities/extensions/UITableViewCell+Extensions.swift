ab9e1276f476be64d1077f2092503dde98dd2613
ko kyaw <kyaw.codes@gmail.com>
2021-05-04 21:26:49 +0630

    Set up MovieDetail UI

--- 02.The Movies/02.The Movies/utilities/extensions/UITableViewCell+Extensions.swift
+++ 02.The Movies/02.The Movies/utilities/extensions/UITableViewCell+Extensions.swift
@@ -19,3 +19,15 @@ extension UITableViewCell {
     }
     
 }
+
+extension UIViewController {
+    
+    func dequeueCollectionViewCell<T: UICollectionViewCell>(ofType: T.Type, with collectionView: UICollectionView, for indexPath: IndexPath, _ setupCell: ((T) -> Void) = {_ in}) -> T {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: T.self), for: indexPath) as? T else {
+            fatalError("ERROR: Fail to cast the given cell into \(T.self)")
+        }
+        setupCell(cell)
+        return cell
+    }
+}
--- 02.The Movies/02.The Movies/view controllers/MovieDetailViewController.swift
+++ 02.The Movies/02.The Movies/view controllers/MovieDetailViewController.swift
@@ -9,10 +9,61 @@ import UIKit
 
 class MovieDetailViewController: UIViewController, Storyboarded {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var playTrailerButton: UIButton!
+    @IBOutlet weak var rateMovieButton: UIButton!
+    
+    @IBOutlet weak var actorsCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupGradient()
+        
+        playTrailerButton.layer.cornerRadius = playTrailerButton.frame.height / 2
+        rateMovieButton.layer.borderColor = UIColor.white.cgColor
+        rateMovieButton.layer.borderWidth = 1
+        rateMovieButton.layer.cornerRadius = rateMovieButton.frame.height / 2
+        
+        actorsCollectionView.dataSource = self
+        actorsCollectionView.delegate = self
+        actorsCollectionView.registerCellWithNib(BestActorsCollectionViewCell.self)
     }
     
+    private func setupGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.init(named: "Color_Dark_Blue")!.cgColor]
+        gradientLayer.locations = [0, 0.9]
+        backgroundImageView.layer.addSublayer(gradientLayer)
+
+        let gradientHeight = backgroundImageView.frame.height * 0.5
+        gradientLayer.frame = CGRect(x: 0, y: backgroundImageView.frame.height - gradientHeight, width: view.frame.width, height: gradientHeight)
+    }
+    
+    @IBAction func onBackTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension MovieDetailViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = dequeueCollectionViewCell(ofType: BestActorsCollectionViewCell.self, with: collectionView, for: indexPath)
+        cell.actorActionDelegate = self
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: collectionView.frame.width / 2.5, height: collectionView.frame.height)
+    }
+}
+
+extension MovieDetailViewController : ActorActionDelegate {
+    func onFavouriteTap(isFavourite: Bool) {
+        // DO SOMETHING
+    }
 }
